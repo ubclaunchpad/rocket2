858612f59002291f63cc4de4454175f5047d2bed
Russell Blickhan <rwblickhan@gmail.com>
2018-09-30 22:08:55 -0700

    Fix pydocstyle errors in setup code

--- model/permissions.py
+++ model/permissions.py
@@ -1,7 +1,10 @@
+"""Data model to represent permissions."""
 from enum import Enum
 
 
 class Permissions(Enum):
+    """Enum to represent possible permissions levels."""
+
     member = 1
     team_lead = 2
     admin = 3
---
+++ model/team.py
@@ -0,0 +1,1 @@
+"""Data model to represent a team."""
