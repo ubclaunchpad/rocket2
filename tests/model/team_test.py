6cde3239da9f89897db237ef77cc502a9177f92b
rmcreyes <robinreyes1998@yahoo.com>
2018-10-02 20:57:54 -0700

    add docstrings and change test names

--- tests/model/team_test.py
+++ tests/model/team_test.py
@@ -1,57 +1,64 @@
-# content of test_sample.py
+"""Test the data model for a team."""
 import uuid
-import sys
 from model.team import Team
-sys.path.append('..')
 
 
-def test0():
+def test_get_display_name():
+    """Test the Team class method get_display_name()."""
     team = Team("team")
     assert team.get_display_name() == "team"
 
 
-def test1():
+def test_set_display_name():
+    """Test the Team class method set_display_name(display_name)."""
     team = Team("a")
     team.set_display_name("b")
     assert team.get_display_name() == "b"
 
 
-def test2():
+def test_get_team_id():
+    """Test the Team class method get_team_id()."""
     team_a = Team("a")
     team_b = Team("b")
     assert team_a.get_team_id() != team_b.get_team_id()
 
 
-def test3():
+def test_get_platform():
+    """Test the Team class method get_platform()."""
     team = Team("team")
     assert team.get_platform() == ""
 
 
-def test4():
+def test_set_platform():
+    """Test the Team class method set_platform(platform)."""
     team = Team("team")
     team.set_platform("web")
     assert team.get_platform() == "web"
 
 
-def test5():
+def test_get_members():
+    """Test the Team class method get_members()."""
     team = Team("team")
     assert team.get_members() == set()
 
 
-def test6():
+def test_discard_member():
+    """Test the Team class method discard_member(uuid)."""
     team = Team("team")
     team.discard_member(uuid.uuid4())
     assert team.get_members() == set()
 
 
-def test7():
+def test_add_member():
+    """Test the Team class method add_member(uuid)."""
     team = Team("team")
     new_uuid = uuid.uuid4()
     team.add_member(new_uuid)
     assert new_uuid in team.get_members()
 
 
-def test8():
+def test_manage_member():
+    """Test Team class method discard_member(uuid) and add_member(uuid)."""
     team = Team("team")
     new_uuid = uuid.uuid4()
     team.add_member(new_uuid)
@@ -59,25 +66,29 @@ def test8():
     assert (new_uuid in team.get_members()) is False
 
 
-def test9():
+def test_is_member_1():
+    """Test if is_member(uuid) can tell if a uuid is not part of the team."""
     team = Team("team")
     new_uuid = uuid.uuid4()
     assert team.is_member(new_uuid) is False
 
 
-def test10():
+def test_is_member_2():
+    """Test if is_member(uuid) can tell if a uuid is a part of the team."""
     team = Team("team")
     new_uuid = uuid.uuid4()
     team.add_member(new_uuid)
     assert team.is_member(new_uuid)
 
 
-def test11():
+def test_get_github_team_name():
+    """Test the Team class method set_github_team_name()."""
     team = Team("team")
     assert team.get_github_team_name() == ""
 
 
-def test12():
+def test_set_github_team_name():
+    """Test the Team class method set_github_team_name(github_team_name)."""
     team = Team("team")
     team.set_github_team_name("brusselsprouts")
     assert team.get_github_team_name() == "brusselsprouts"
