2ffffba9e49bbeff7214beb33c0320f9cf15ea1d
Cheuk Yin Ng <cheukyin699@yahoo.com>
2018-10-13 14:54:23 -0700

    Renames facade to better name

---
+++ db/facade.py
@@ -0,0 +1,32 @@
+'''
+Database Facade
+'''
+
+
+class DatabaseFacade:
+    '''
+    Self-explanatory
+    '''
+    def __init__():
+        '''
+        TODO
+        '''
+        pass
+
+    def store_user(user):
+        '''
+        TODO
+        '''
+        pass
+
+    def retrieve_user(id):
+        '''
+        TODO
+        '''
+        pass
+
+    def query_user(parameter):
+        '''
+        TODO
+        '''
+        pass
