cefc6b16e2c66164b85b660ad44e8d382d3fcd0a
e7u1b <jacqueschen1@gmail.com>
2018-10-17 21:23:06 -0700

    Fixed pycodestyle line too long error

--- tests/command/commands/user_test.py
+++ tests/command/commands/user_test.py
@@ -2,13 +2,18 @@
 from command.commands.user import UserCommand
 
 
-help_text = "User Command Reference:\n\n @rocket user\n\n Options:\n\n edit \n --name NAME\n" \
-             " --email ADDRESS\n --pos YOURPOSITION\n --major YOURMAJOR\n --bio YOURBIO\n 'edit properties of your" \
-             "Launch Pad profile\n surround arguments with spaces with single quotes'" \
-             "\n ADMIN/TEAM LEAD ONLY option: --member MEMBER_ID\n 'edit properties of another" \
-             "user's Launch Pad profile'\n\n view MEMBER_ID\n 'view information about a user'\n\n " \
+help_text = "User Command Reference:\n\n @rocket user\n\n Options:\n\n" \
+             " edit \n --name NAME\n" \
+             " --email ADDRESS\n --pos YOURPOSITION\n" \
+             " --major YOURMAJOR\n --bio YOURBIO\n" \
+             " 'edit properties of your Launch Pad profile\n" \
+             " surround arguments with spaces with single quotes'" \
+             "\n ADMIN/TEAM LEAD ONLY option: --member MEMBER_ID\n" \
+             " 'edit properties of another user's Launch Pad profile'\n\n" \
+             " view MEMBER_ID\n 'view information about a user'\n\n " \
              "help\n 'outputs options for user commands'\n\n " \
-             "ADMIN ONLY\n\n delete MEMBER_ID\n 'permanently delete member's Launch Pad profile'"
+             "ADMIN ONLY\n\n delete MEMBER_ID\n" \
+             " 'permanently delete member's Launch Pad profile'"
 
 
 def test_get_command_name():
