88b63c6846cd37ba9c23d535e8d54ae757923200
Michael Englo <menglo@optimalefficiency.net>
2018-10-20 12:56:28 -0700

    resolve pr comment

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,17 @@
 """Utility class for interacting with Slack API"""
 import os
 from slackclient import SlackClient
-
-slack_token = os.environ["SLACK_API_TOKEN"]
 class Bot:
   """Utility class for interacting with Slack API"""
   def __init__(self):
     """Initialize Bot by creating a SlackClient Object"""
+    
+    token_not_found_message = "Token Not Found. Please add SLACK_API_TOKEN to your .env"
+    slack_token = os.getenv("SLACK_API_TOKEN", token_not_found_message)
+
+    if slack_token == token_not_found_message:
+      raise Exception(token_not_found_message)
+
     self.sc = SlackClient(slack_token)
 
   def send_dm(self, message, slack_user_id):
@@ -18,9 +23,10 @@ class Bot:
     )
 
     if not response['ok']:
-      raise Exception('API Call error: {}'.format(response["error"]))
-
-    return
+      print('API Call error: {}'.format(response["error"]))
+      return False
+    
+    return True
 
   def send_to_channel(self, message, channel_name):
     """Send message to channel with name channel_name"""
@@ -31,10 +37,11 @@ class Bot:
     )
 
     if not response['ok']:
-      raise Exception('API Call error: {}'.format(response["error"]))
+      print('API Call error: {}'.format(response["error"]))
+      return False
 
-    return
+    return True
 
 
 # Example of how we are going to use SlackBot interface
-# Bot().send_dm("Hello <@UD8UCTN05>", "UD8UCTN05")
+Bot().send_dm("Hello <@UD8UCTN05>", "UD8UCTN05")
