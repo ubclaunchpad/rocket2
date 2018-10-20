22fd8bce275ea7956d400e3282c492dbba292754
Michael Englo <menglo@optimalefficiency.net>
2018-10-20 03:25:25 -0700

    add test and complete functionality of send dm

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,36 @@
+"""Utility class for interacting with Slack API"""
 import os
 from slackclient import SlackClient
 
-slack_token = os.environ["SLACK_BOT_TOKEN"]
+# slack_token = os.environ["SLACK_API_TOKEN"]
+slack_token = "xoxp-450677644563-450964940005-450690754371-72246adde9effadb4b6d89791edb6316"
 
 class Bot:
-
+  """Utility class for interacting with Slack API"""
   def __init__(self):
+    """Initialize Bot by creating a SlackClient Object"""
     self.sc = SlackClient(slack_token)
 
-  def sendMsg(self, message, channel):
-    self.sc.api_call(
+  def send_dm(self, message, slack_user_id):
+    """Send direct message to user with id of slack_user_id"""
+    response = self.sc.api_call(
       "chat.postMessage",
-      channel=channel,
+      channel=slack_user_id,
+      text=message
+    )
+
+    if not response['ok']:
+      raise Exception('API Call error: {}'.format(response["error"]))
+
+    return
+
+  def send_to_channel(self, message, channel_name):
+    response = self.sc.api_call(
+      "chat.postMessage",
+      channel=channel_name,
       text=message
     )
 
 
 # Example of how we are going to use SlackBot interface
-# Bot().sendMsg("Hello", "#random")
+# Bot().send_dm("Hello <@UD8UCTN05>", "UD8UCTN05")
---
+++ tests/bot/bot_test.py
@@ -0,0 +1,11 @@
+"""Test Bot Class."""
+from bot.bot import Bot
+
+def test_send_dm():
+    """Test the Bot class method send_dm()."""
+    bot = Bot()
+    try:
+        bot.send_dm("Hahahaha", "UD8UCTN05")
+        assert True
+    except:
+        assert False
