ad43ad9269f0d8e9c518404d7ac115562075d3dc
Michael Englo <menglo@optimalefficiency.net>
2018-11-03 11:50:50 -0700

    resolve pycode sytle errors

--- bot/bot.py
+++ bot/bot.py
@@ -1,36 +1,38 @@
 """Utility class for interacting with Slack API"""
 import os
 from slackclient import SlackClient
+
+
 class Bot:
-  """Utility class for interacting with Slack API"""
+    """Utility class for interacting with Slack API"""
 
-  def __init__(self, sc):
-    """Initialize Bot by creating a SlackClient Object"""
-    self.sc = sc
+    def __init__(self, sc):
+        """Initialize Bot by creating a SlackClient Object"""
+        self.sc = sc
 
-  def send_dm(self, message, slack_user_id):
-    """Send direct message to user with id of slack_user_id"""
-    response = self.sc.api_call(
-      "chat.postMessage",
-      channel=slack_user_id,
-      text=message
-    )
-    if not response['ok']:
-      print('API Call error: {}'.format(response["error"]))
-      return False
-    return True
+    def send_dm(self, message, slack_user_id):
+        """Send direct message to user with id of slack_user_id"""
+        response = self.sc.api_call(
+                            "chat.postMessage",
+                            channel=slack_user_id,
+                            text=message
+                            )
+        if not response['ok']:
+            print('API Call error: {}'.format(response["error"]))
+            return False
+        return True
 
-  def send_to_channel(self, message, channel_name):
-    """Send message to channel with name channel_name"""
-    response = self.sc.api_call(
-      "chat.postMessage",
-      channel=channel_name,
-      text=message
-    )
-    if not response['ok']:
-      print('API Call error: {}'.format(response["error"]))
-      return False
-    return True
+    def send_to_channel(self, message, channel_name):
+        """Send message to channel with name channel_name"""
+        response = self.sc.api_call(
+            "chat.postMessage",
+            channel=channel_name,
+            text=message
+        )
+        if not response['ok']:
+            print('API Call error: {}'.format(response["error"]))
+            return False
+        return True
 
 
 # Example of how we are going to use SlackBot interface
--- tests/bot/bot_test.py
+++ tests/bot/bot_test.py
@@ -1,27 +1,34 @@
 """Test Bot Class."""
 from bot.bot import Bot
 from slackclient import SlackClient
+from unittest import mock, TestCase
 
-from unittest import mock, TestCase 
 
 class TestBot(TestCase):
-    
+
     def setUp(self):
         self.mock_sc = mock.MagicMock(SlackClient)
-        
 
     def test_send_dm(self):
         """Test the Bot class method send_dm()."""
         bot = Bot(self.mock_sc)
         self.mock_sc.api_call = mock.MagicMock(return_value={"ok": True})
-        
+
         bot.send_dm("Hahahaha", "UD8UCTN05")
-        self.mock_sc.api_call.assert_called_with('chat.postMessage', text="Hahahaha", channel="UD8UCTN05")
+        self.mock_sc.api_call.assert_called_with(
+                                        'chat.postMessage',
+                                        text="Hahahaha",
+                                        channel="UD8UCTN05"
+                                        )
 
     def test_send_to_channel(self):
         """Test the Bot class method send_to_channel()."""
         bot = Bot(self.mock_sc)
         self.mock_sc.api_call = mock.MagicMock(return_value={"ok": True})
-        
+
         bot.send_to_channel("Hahahaha", "#random")
-        self.mock_sc.api_call.assert_called_with('chat.postMessage', text="Hahahaha", channel="#random")
+        self.mock_sc.api_call.assert_called_with(
+                                        'chat.postMessage',
+                                        text="Hahahaha",
+                                        channel="#random"
+                                        )
