000eae90f52a555e392204082a2733cea5e8fc82
Cheuk Yin Ng <chucksys88@gmail.com>
2018-11-03 13:50:46 -0700

    Pytest tests for dynamodb things is optional
    It still runs by default, but allows you the option of
    skipping/deselecting it if you run `pytest`.

--- scripts/build_check.sh
+++ scripts/build_check.sh
@@ -3,4 +3,14 @@ set -euxo pipefail
 pipenv run pycodestyle .
 pipenv run pydocstyle .
 mdl .
-pipenv run pytest tests/
+
+# We use nmap to check if dynamodb is running locally
+# TODO: later should combine this with environmental variable checking (to see
+# if we can use a remote server instead).
+# XXX: Find a better way to check if dynamodb is running locally
+if nmap localhost | egrep "8000.*http-alt"; then
+    pipenv run pytest tests/
+else
+    printf "Warning: DynamoDB not detected. Running without the tests.\n"
+    pipenv run pytest tests/ -m "not db"
+fi
