0997c78cb23140ec5c3de12ca84920abf44c84c2
Cheuk Yin Ng <chucksys88@gmail.com>
2018-11-04 22:01:36 -0800

    Fix #63
    Add default handler for user, and also add test for that one case.
    
    If the parser errors out, help is printed instead of exiting.

--- tests/command/commands/user_test.py
+++ tests/command/commands/user_test.py
@@ -1,19 +1,6 @@
 """Test user command parsing."""
 from command.commands.user import UserCommand
 
-help_text = "User Command Reference:\n\n @rocket user\n\n Options:\n\n" \
-             " edit \n --name NAME\n" \
-             " --email ADDRESS\n --pos YOURPOSITION\n" \
-             " --major YOURMAJOR\n --bio YOURBIO\n" \
-             " 'edit properties of your Launch Pad profile\n" \
-             " surround arguments with spaces with single quotes'" \
-             "\n ADMIN/TEAM LEAD ONLY option: --member MEMBER_ID\n" \
-             " 'edit properties of another user's Launch Pad profile'\n\n" \
-             " view MEMBER_ID\n 'view information about a user'\n\n " \
-             "help\n 'outputs options for user commands'\n\n " \
-             "ADMIN ONLY\n\n delete MEMBER_ID\n" \
-             " 'permanently delete member's Launch Pad profile'"
-
 
 def test_get_command_name():
     """Test user command get_name method."""
@@ -24,7 +11,19 @@ def test_get_command_name():
 def test_get_help():
     """Test user command get_help method."""
     testcommand = UserCommand()
-    assert testcommand.get_help() == help_text
+    assert testcommand.get_help() == UserCommand.help
+
+
+def test_handle_nosubs():
+    """Test user with no sub-parsers."""
+    testcommand = UserCommand()
+    assert testcommand.handle('user') == UserCommand.help
+
+
+def test_handle_bad_args():
+    """Test user with invalid arguments."""
+    testcommand = UserCommand()
+    assert testcommand.handle('user geese-say-honk') == UserCommand.help
 
 
 def test_handle_view():
@@ -36,7 +35,7 @@ def test_handle_view():
 def test_handle_help():
     """Test user command help parser."""
     testcommand = UserCommand()
-    assert testcommand.handle('user help') == help_text
+    assert testcommand.handle('user help') == UserCommand.help
 
 
 def test_handle_delete():
