1341bbd481cb100b5fcc8caae06b88fcf1ebc8fa
jacqueschen1 <jacqueschen1@gmail.com>
2019-01-12 14:04:44 -0800

    fixed pycodestyle error

--- tests/model/user_test.py
+++ tests/model/user_test.py
@@ -139,7 +139,8 @@ def test_print():
     user.set_permissions_level(Permissions.admin)
     assert str(user) == "{'_User__slack_id': 'U0G9QF9C6', '_User__name': ''," \
                         " '_User__email': 'email@email.com'," \
-                        " '_User__github_username': '', '_User__github_id': '', " \
+                        " '_User__github_username': '', " \
+                        "'_User__github_id': '', " \
                         "'_User__major': ''," \
                         " '_User__position': '', '_User__biography':" \
                         " 'bio test', '_User__image_url': ''," \
