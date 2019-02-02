2d1da2e7beca90eaa2482078f56ad4f9e72d54d3
ravenCondol <ravencondol@hotmail.co.uk>
2019-02-02 15:36:01 -0800

    Fixed weird make_core test that wasn't passing on Travis for some reason?

--- tests/factory/test_factories.py
+++ tests/factory/test_factories.py
@@ -3,7 +3,8 @@ from factory import *
 from db.facade import DBFacade
 from command.core import Core
 import pytest
-
+from unittest import mock
+from interface.github import GithubInterface
 
 @pytest.mark.db
 def test_make_core():
@@ -15,5 +16,5 @@ def test_make_core():
             'teams_table': 'teams_test'
         }
     }
-    core = make_core(test_config)
+    core = make_core(test_config, mock.MagicMock(GithubInterface))
     assert isinstance(core, Core)
