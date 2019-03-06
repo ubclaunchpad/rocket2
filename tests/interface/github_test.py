8a3606507c68ce8cf9d110fe832b3b9e9939ada5
Russell Blickhan <rwblickhan@gmail.com>
2019-03-06 14:53:39 -0800

    Fix team member tests that changed before GithubAppInterface was introduced

--- tests/interface/github_test.py
+++ tests/interface/github_test.py
@@ -20,16 +20,16 @@ class TestGithubInterface(TestCase):
                                               "ubclaunchpad")
 
         # make mock team
-        self.mock_team = mock.MagicMock(Team.Team)
+        self.mock_team = MagicMock(Team.Team)
 
-        self.mock_github.get_team = mock.MagicMock(side_effect={
+        self.mock_github.get_team = MagicMock(side_effect={
             'brussels-sprouts': self.mock_team,
         }.get)
 
-        self.test_user = mock.MagicMock(NamedUser.NamedUser)
+        self.test_user = MagicMock(NamedUser.NamedUser)
         self.test_user.name = 'member_username'
-        self.mock_team.get_members = mock.MagicMock(
-                                        return_value=[self.test_user])
+        self.mock_team.get_members = MagicMock(
+            return_value=[self.test_user])
 
     def test_org_add_member(self):
         """Test GithubInterface method org_add_member."""
@@ -219,46 +219,44 @@ class TestGithubInterface(TestCase):
         except GithubAPIException as e:
             pass
 
-# -------------------------------------------------------------
-# --------------- Tests related to team members ---------------
-# -------------------------------------------------------------
+    # -------------------------------------------------------------
+    # --------------- Tests related to team members ---------------
+    # -------------------------------------------------------------
 
     def test_tmem_list_team_members(self):
         """Test if list_team_members returns the right team members."""
-        test_team_members_list = [mock.MagicMock(
-                                    NamedUser.NamedUser)]
-        self.mock_team.list_team_members = mock.MagicMock(
-                                            return_value=test_team_members_list
-                                            )
-        self.test_bot.list_team_members('brussels-sprouts')
+        test_team_members_list = [MagicMock(NamedUser.NamedUser)]
+        self.mock_team.list_team_members = MagicMock(
+            return_value=test_team_members_list
+        )
+        self.test_interface.list_team_members('brussels-sprouts')
         self.mock_github.get_team.assert_called_once_with('brussels-sprouts')
 
     def test_tmem_get_team_member(self):
         """Test if method gets the correct member when member exists."""
-        assert self.test_bot.\
-            get_team_member(
-                self.test_user.name, 'brussels-sprouts') is self.test_user
+        assert self.test_interface.get_team_member(
+            self.test_user.name,
+            'brussels-sprouts') is self.test_user
 
     def test_tmem_get_nonexistent_team_member(self):
         """Test if raises GithubException when memeber does not exist."""
         with self.assertRaises(GithubAPIException):
-            self.test_bot.\
-                    get_team_member(
-                        'inexistent_username', 'brussels-sprouts')
+            self.test_interface.get_team_member('inexistent_username',
+                                                'brussels-sprouts')
 
     def test_tmem_add_team_member(self):
         """Test if a user is added to a team properly."""
-        self.mock_github.get_user = mock.MagicMock(return_value=self.test_user)
-        self.mock_team.add_membership = mock.MagicMock()
-
-        self.test_bot.add_team_member('member_username', 'brussels-sprouts')
+        self.mock_github.get_user = MagicMock(return_value=self.test_user)
+        self.mock_team.add_membership = MagicMock()
+        self.test_interface.add_team_member('member_username',
+                                            'brussels-sprouts')
         self.mock_team.add_membership.assert_called_once_with(self.test_user)
 
     def test_tmem_remove_team_member(self):
         """Test if the user removed is no longer in the team."""
-        self.mock_team.remove_membership = mock.MagicMock()
-        self.mock_github.get_user = mock.MagicMock(return_value=self.test_user)
-        self.test_bot.remove_team_member(
-            self.test_user.name, 'brussels-sprouts')
-        self.mock_team.remove_membership.assert_called_once_with(
-                                                        self.test_user)
+        self.mock_team.remove_membership = MagicMock()
+        self.mock_github.get_user = MagicMock(return_value=self.test_user)
+        self.test_interface.remove_team_member(self.test_user.name,
+                                               'brussels-sprouts')
+        self.mock_team.remove_membership. \
+            assert_called_once_with(self.test_user)
