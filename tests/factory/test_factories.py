3e216b88d1caa88bda1dfdca08e5b4d034da1c56
Michael Englo <michaelenglo10@gmail.com>
2019-03-30 03:26:28 -0700

    revert back commented code

--- tests/factory/test_factories.py
+++ tests/factory/test_factories.py
@@ -6,16 +6,16 @@ from interface.github import GithubInterface
 from unittest import mock
 
 
-# @pytest.mark.db
-# def test_make_core():
-#     """Test the make_core function."""
-#     test_config = {
-#         'testing': True,
-#         'aws': {
-#             'users_table': 'users_test',
-#             'teams_table': 'teams_test',
-#             'projects_table': 'projects_test'
-#         }
-#     }
-#     core = make_core(test_config, mock.MagicMock(GithubInterface))
-#     assert isinstance(core, Core)
+@pytest.mark.db
+def test_make_core():
+    """Test the make_core function."""
+    test_config = {
+        'testing': True,
+        'aws': {
+            'users_table': 'users_test',
+            'teams_table': 'teams_test',
+            'projects_table': 'projects_test'
+        }
+    }
+    core = make_core(test_config, mock.MagicMock(GithubInterface))
+    assert isinstance(core, Core)
