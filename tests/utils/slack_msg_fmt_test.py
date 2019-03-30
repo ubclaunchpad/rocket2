8a590b48189f2e788e99f454ed47500fe0fb9260
Michael Englo <michaelenglo10@gmail.com>
2019-03-30 03:36:13 -0700

    add docstrings

--- tests/utils/slack_msg_fmt_test.py
+++ tests/utils/slack_msg_fmt_test.py
@@ -4,21 +4,28 @@ from unittest import TestCase
 
 
 class TestGithubInterface(TestCase):
+    """Unittest TestCase for testing SlackMsgFmt."""
+
     def setUp(self):
+        """Setup testing environment."""
         self.sfmt = SlackMsgFmt()
 
     def test_code(self):
+        """Test code formatting."""
         code = 'map(lambda x: x)'
         assert self.sfmt.code(code) == f"`{code}`"
 
     def test_code_block(self):
+        """Test code block formatting."""
         code_block = 'map(lambda x: x)\nmap(lambda x: x)'
         assert self.sfmt.code_block(code_block) == f"```\n{code_block}\n```"
 
     def test_quote(self):
+        """Test quote formatting."""
         quote = 'this is\na multi-line\n quote.'
         assert self.sfmt.quote(quote) == f"> {quote}\n"
 
     def test_emph(self):
+        """Test emph formatting."""
         emph = 'THIS IS VERY IMPORTANT!!!\nPLEASE READ IT!!!'
         assert self.sfmt.emph(emph) == f"*{emph}*"
--- utils/slack_msg_fmt.py
+++ utils/slack_msg_fmt.py
@@ -2,14 +2,20 @@
 
 
 class SlackMsgFmt():
+    """Utils class for Slack Message Format."""
+
     def code(self, str):
+        """Format code."""
         return f"`{str}`"
 
     def code_block(self, str):
+        """Format code block."""
         return f"```\n{str}\n```"
 
     def quote(self, str):
+        """Format quote."""
         return f"> {str}\n"
 
     def emph(self, str):
+        """Format emph."""
         return f"*{str}*"
