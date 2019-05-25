30388844df3d58a5adc0d4714d57dbc1c9768c9c
Omar <omar2535@gmail.com>
2019-05-25 11:21:58 -0700

    moving karma add to karma command

--- command/commands/karma.py
+++ command/commands/karma.py
@@ -1,33 +1,49 @@
 import argparse
 import logging
-import shlex
+import pdb
 import re
-
+import shlex
+from command import ResponseTuple
+from model.user import User
 
 class KarmaCommand:
     """karma command parser"""
 
     command_name = "karma"
     help = "karma command reference:\n\n /rocket karma"\
-        "\n\nOptions:\n\n" \
-        "user"
+            "\n\nOptions:\n\n" \
+            "user"
     lookup_error = "User doesn't exist"
     desc = "for dealing with " + command_name
+    karma_add_amount = 1
 
     def __init__(self, db_facade):
-        """Initialize karma command"""
-        logging.info("Starting karma command initializer")
-        self.parser = argparse.ArgumentParser(prog="karma")
-        self.parser.add_argument("karma")
-        self.facade = db_facade
+            """Initialize karma command"""
+            logging.info("Starting karma command initializer")
+            self.parser = argparse.ArgumentParser(prog="karma")
+            self.parser.add_argument("karma")
+            self.facade = db_facade
 
     def handle(self, command, user_id):
-        """Handle command by splitting into substrings"""
-        logging.debug('Handling karma Command')
-        command_arg = shlex.split(command)
+            """Handle command by splitting into substrings"""
+            logging.debug('Handling karma Command')
+            command_arg = shlex.split(command)
 
-        if(self.is_user(command_arg[0])):
-            return self.short_karma_helper(user_id, command_arg[0], command_arg[1])
-        elif(command_arg[0] == "karma" and self.is_user(command_arg[1])):
-            reciever = self.parse_reciever(command_arg)
-            return self.karma_helper(user_id, reciever, 1)
+            if(self.is_user(command_arg[0])):
+                    return self.short_karma_helper(user_id, command_arg[0], command_arg[1])
+            elif(command_arg[0] == "karma" and self.is_user(command_arg[1])):
+                    reciever = self.parse_reciever(command_arg)
+                    return self.karma_helper(user_id, reciever, 1)
+
+    def add_karma(self, giver_id, receiver_id):
+        """Gives karma from giver_id to receiver_id"""
+        logging.info("giving karma to " + receiver_id)
+        if(giver_id == receiver_id):
+            return "cannot give karma to self", 200
+        try:
+            user = self.facade.retrieve(User, receiver_id)
+            user.karma += self.karma_add_amount
+            self.facade.store(user)
+            return f"gave 1 karma to {user.name}", 200
+        except:
+            return self.lookup_error, 200
--- command/commands/mention.py
+++ command/commands/mention.py
@@ -1,10 +1,14 @@
-from model.user import User
-from typing import Dict, cast
 import argparse
 import logging
-import shlex
-import re
 import pdb
+import re
+import shlex
+from typing import Dict, cast
+from command import ResponseTuple
+from command.commands.karma import KarmaCommand
+from db.facade import DBFacade
+from model.user import User
+
 
 class MentionCommand:
     """Mention command parser"""
@@ -15,16 +19,15 @@ class MentionCommand:
         "user"
     lookup_error = "User doesn't exist"
     desc = "for dealing with " + command_name
-    karma_add_amount = 1
 
-    def __init__(self, db_facade):
+    def __init__(self, db_facade: DBFacade) -> None:
         """Initialize Mention command"""
         logging.info("Starting Mention command initializer")
         self.parser = argparse.ArgumentParser(prog="Mention")
         self.parser.add_argument("Mention")
         self.facade = db_facade
 
-    def handle(self, command, user_id):
+    def handle(self, command: str, user_id: str) -> ResponseTuple:
         """Handle command by splitting into substrings"""
         logging.debug('Handling Mention Command')
         command_arg = shlex.split(command)
@@ -33,16 +36,7 @@ class MentionCommand:
         elif(command_arg[1] == "help"):
             return self.help, 200
         elif(command_arg[1] == '++'):
-            return self.karma_mention_helper(user_id, command_arg[0], self.karma_add_amount)
+            return self.karma_mention_helper(user_id, command_arg[0])
 
-    def karma_mention_helper(self, giver, receiver, amount):
-        logging.info("giving karma to " + receiver)
-        if(giver == receiver):
-            return "cannot give karma to self", 200
-        try:
-            user = self.facade.retrieve(User, receiver)
-            user.karma += amount
-            self.facade.store(user)
-            return f"gave 1 karma to {user.name}", 200
-        except:
-            return self.lookup_error, 200
+    def karma_mention_helper(self, giver_id: str, receiver_id: str) -> ResponseTuple:
+      return KarmaCommand(self.facade).add_karma(giver_id, receiver_id)
