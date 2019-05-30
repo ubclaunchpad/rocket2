5d3157916be57c46b6f43da726f046a1f325a0a1
Cheuk Yin Ng <chucksys88@gmail.com>
2019-05-29 21:14:38 -0700

    Add files change and silence errors in grep

--- scripts/setup_credentials.sh
+++ scripts/setup_credentials.sh
@@ -10,42 +10,58 @@ fi
 problems=0
 CREDS=$(grep creds_path "$CONF" | cut -d ' ' -f 3 | tr -d '"')
 SIGNING=$(grep signing_key_path "$CONF" | cut -d ' ' -f 3 | tr -d '"')
+declare -A FILESCHANGED         # We use a map as a set
+
+# Check to see if the credentials directory exists, and make it if not.
+# We don't count this as a problem; if the user does not have the directory in
+# place, there are bigger problems with their directory structures anyway.
+if [[ ! -d "$CREDS" ]]; then
+    mkdir "$CREDS"
+fi
 
 # Check to see if the variables are there, and insert them if not
 # ------------- AWS.TOML
-if ! grep -q access_key_id "$CREDS/aws.toml"; then
+if ! grep -sq access_key_id "$CREDS/aws.toml"; then
     echo 'access_key_id = "YOUR_KEY_ID"' >> "$CREDS/aws.toml"
+    FILESCHANGED["$CREDS/aws.toml"]=1
     ((problems++))
 fi
-if ! grep -q secret_access_key "$CREDS/aws.toml"; then
+if ! grep -sq secret_access_key "$CREDS/aws.toml"; then
     echo 'secret_access_key = "YOUR_SECRET_KEY"' >> "$CREDS/aws.toml"
+    FILESCHANGED["$CREDS/aws.toml"]=1
     ((problems++))
 fi
 # ------------- GITHUB.TOML
-if ! grep -q app_id "$CREDS/github.toml"; then
+if ! grep -sq app_id "$CREDS/github.toml"; then
     echo 'app_id = -1' >> "$CREDS/github.toml"
+    FILESCHANGED["$CREDS/github.toml"]=1
     ((problems++))
 fi
-if ! grep -q organization "$CREDS/github.toml"; then
+if ! grep -sq organization "$CREDS/github.toml"; then
     echo 'organization = "YOUR_ORGANIZATION"' >> "$CREDS/github.toml"
+    FILESCHANGED["$CREDS/github.toml"]=1
     ((problems++))
 fi
-if ! grep -q webhook_secret "$CREDS/github.toml"; then
+if ! grep -sq webhook_secret "$CREDS/github.toml"; then
     echo 'webhook_secret = "NANI DA HECK"' >> "$CREDS/github.toml"
+    FILESCHANGED["$CREDS/github.toml"]=1
     ((problems++))
 fi
 # ------------- SLACK.TOML
-if ! grep -q signing_secret "$CREDS/slack.toml"; then
+if ! grep -sq signing_secret "$CREDS/slack.toml"; then
     echo 'signing_secret = "SECRET SIGN"' >> "$CREDS/slack.toml"
+    FILESCHANGED["$CREDS/slack.toml"]=1
     ((problems++))
 fi
-if ! grep -q api_token "$CREDS/slack.toml"; then
+if ! grep -sq api_token "$CREDS/slack.toml"; then
     echo 'api_token = "API TOKEN"' >> "$CREDS/slack.toml"
+    FILESCHANGED["$CREDS/slack.toml"]=1
     ((problems++))
 fi
 # ------------- SIGNING_KEY
 if [[ ! -e "$SIGNING" ]]; then
     touch "$SIGNING"
+    FILESCHANGED["$SIGNING"]=1
     ((problems++))
 fi
 
@@ -55,4 +71,10 @@ if [[ "$problems" -eq "0" ]]; then
 else
     echo "We found $problems issues in your credentials."
     echo "We added the required fields and files."
+    echo ""
+    echo "Below are a list of files that were changed."
+    echo ""
+    for key in "${!FILESCHANGED[@]}"; do
+        printf "\t%s\n" "$key"
+    done
 fi
