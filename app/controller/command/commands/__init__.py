e22fd4cdf13f3714729c6c5799831bcf00182797
Robin Reyes <rmcreyes.eng@gmail.com>
2019-09-10 23:17:00 -0700

    Replace UnionCommands with Command ABC

--- app/controller/command/commands/__init__.py
+++ app/controller/command/commands/__init__.py
@@ -1,8 +1,5 @@
 """Pack the modules contained in the commands directory."""
-from typing import Union
 from app.controller.command.commands.team import TeamCommand
 from app.controller.command.commands.user import UserCommand
 from app.controller.command.commands.token import TokenCommand
 from app.controller.command.commands.project import ProjectCommand
-
-UnionCommands = Union[TeamCommand, UserCommand, TokenCommand, ProjectCommand]
