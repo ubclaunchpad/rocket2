31da5cb5f2ac2391305b9161bb61600526d9ee17
Cheuk Yin Ng <chucksys88@gmail.com>
2019-09-21 23:07:26 -0700

    Have build script work everywhere

--- scripts/build_check.sh
+++ scripts/build_check.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+pushd "${REPO_ROOT}"
+
 pipenv run pycodestyle .
 pipenv run flake8 .
 pipenv run pydocstyle .
@@ -8,11 +12,15 @@ mdl .
 
 # We use a script to check if dynamodb is running locally
 COV_OPTIONS="--mypy --cov=./ --cov-branch --cov-config .coverageac"
-PORT_BUSY="pipenv run python scripts/port_busy.py 8000"
+PORT_CHECKER="scripts/port_busy.py"
+TESTS="tests/"
+PORT_BUSY="pipenv run python ${PORT_CHECKER} 8000"
 if ${PORT_BUSY}; then
     printf "DynamoDB detected. Running all tests.\n"
-    pipenv run pytest tests/ ${COV_OPTIONS}
+    pipenv run pytest "${TESTS}" ${COV_OPTIONS}
 else
     printf "Warning: DynamoDB not detected. Running without the tests.\n"
-    pipenv run pytest tests/ -m "not db" ${COV_OPTIONS}
+    pipenv run pytest "${TESTS}" -m "not db" ${COV_OPTIONS}
 fi
+
+popd
