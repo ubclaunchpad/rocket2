543400bd0def422f72fc8038b1b6d62386476e3f
Robert Lin <robertlin1@gmail.com>
2019-10-20 13:08:14 -0700

    RandomChannelPromoter: use config.slack_bot_channel as the default channel

--- app/scheduler/modules/random_channel.py
+++ app/scheduler/modules/random_channel.py
@@ -18,6 +18,7 @@ class RandomChannelPromoter(ModuleBase):
                  flask_app: Flask,
                  config: Config):
         """Initialize the object."""
+        self.default_channel = config.slack_bot_channel
         self.bot = Bot(WebClient(config.slack_api_token),
                        config.slack_bot_channel)
 
@@ -35,6 +36,6 @@ class RandomChannelPromoter(ModuleBase):
         channel_id, channel_name = rand_channel['id'], rand_channel['name']
         self.bot.send_to_channel(f'Featured channel of the week: ' +
                                  f'<#{channel_id}|{channel_name}>!',
-                                 '#general')
+                                 self.default_channel)
 
         logging.info(f'Featured #{channel_name}')
