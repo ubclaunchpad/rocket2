a058d72dbfd2d5edcf591fd562c72d462f8adb1f
Robin Reyes <rmcreyes.eng@gmail.com>
2019-10-25 01:59:48 -0700

    Return specific help message for user subcommands

--- tests/app/controller/command/commands/user_test.py
+++ tests/app/controller/command/commands/user_test.py
@@ -50,12 +50,6 @@ class TestUserCommand(TestCase):
         self.assertEqual(self.testcommand.handle('user geese', "U0G9QF9C6"),
                          (self.testcommand.help, 200))
 
-    def test_handle_bad_optional_args(self):
-        """Test user edit with invalid optional arguments."""
-        self.assertEqual(self.testcommand.handle('user edit --biology stuff',
-                                                 "U0G9QF9C6"),
-                         (self.testcommand.help, 200))
-
     def test_handle_add(self):
         """Test user command add method."""
         user_id = "U0G9QF9C6"
@@ -339,3 +333,34 @@ class TestUserCommand(TestCase):
                          (UserCommand.lookup_error, 200))
         self.mock_facade.retrieve.assert_called_once_with(User, "U0G9QF9C6")
         self.mock_facade.store.assert_not_called()
+
+    def test_handle_command_help(self):
+        """Test user command help text."""
+        ret, code = self.testcommand.handle("user help", "U0G9QF9C6")
+        self.assertEqual(ret, self.testcommand.get_help())
+        self.assertEqual(code, 200)
+
+    def test_handle_multiple_subcommands(self):
+        """Test handling multiple observed subcommands."""
+        ret, code = self.testcommand.handle("user edit view", "U0G9QF9C6")
+        self.assertEqual(ret, self.testcommand.get_help())
+        self.assertEqual(code, 200)
+
+    def test_handle_subcommand_help(self):
+        """Test user subcommand help text."""
+        subcommands = list(self.testcommand.subparser.choices.keys())
+        for subcommand in subcommands:
+            command = f"user {subcommand} --help"
+            ret, code = self.testcommand.handle(command, "U0G9QF9C6")
+            self.assertEqual(1, ret.count("usage"))
+            self.assertEqual(code, 200)
+
+            command = f"user {subcommand} -h"
+            ret, code = self.testcommand.handle(command, "U0G9QF9C6")
+            self.assertEqual(1, ret.count("usage"))
+            self.assertEqual(code, 200)
+
+            command = f"user {subcommand} --invalid argument"
+            ret, code = self.testcommand.handle(command, "U0G9QF9C6")
+            self.assertEqual(1, ret.count("usage"))
+            self.assertEqual(code, 200)
