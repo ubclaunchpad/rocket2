261316ad25ae2a302bb0a17a6829de5b1d857359
Robin Reyes <rmcreyes.eng@gmail.com>
2019-11-12 23:37:16 -0800

    Create class for command APIs

---
+++ api/command/core.py
@@ -0,0 +1,9 @@
+"""Encapsulate the common business logic of all command types."""
+
+
+class CommandApis:
+    """Encapsulate the various APIs of each command type."""
+
+    def __init__(self) -> None:
+        """Initialize the dependencies of command APIs."""
+        pass
