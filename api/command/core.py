f6d8daef0417621de5642dc8e348fcbdb22364de
Robin Reyes <rmcreyes.eng@gmail.com>
2019-12-21 04:04:56 -0800

    Reimplement user command logic into generic APIs

--- api/command/core.py
+++ api/command/core.py
@@ -1,9 +1,19 @@
 """Encapsulate the common business logic of all command types."""
+from db.facade import DBFacade
+from interface.github import GithubInterface
+from interface.slack import Bot
+
+from api.command.mixins import UserCommandApis
 
 
-class CommandApis:
+class CommandApis(UserCommandApis):
     """Encapsulate the various APIs of each command type."""
 
-    def __init__(self) -> None:
+    def __init__(self,
+                 db_facade: DBFacade,
+                 gh_interface: GithubInterface,
+                 slack_client: Bot) -> None:
         """Initialize the dependencies of command APIs."""
-        pass
+        self._db_facade = db_facade
+        self._gh_interface = gh_interface
+        self._slack_client = slack_client
