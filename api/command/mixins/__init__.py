c73d854f2bcd49b9d8a8c4cb4779c5ad5ea88b4c
Robin Reyes <rmcreyes.eng@gmail.com>
2020-01-01 20:22:50 -0800

    Reimplement team command logic into generic APIs

--- api/command/mixins/__init__.py
+++ api/command/mixins/__init__.py
@@ -1,4 +1,6 @@
 """Define the APIs of the various command types."""
 import api.command.mixins.user as user
+import api.command.mixins.team as team
 
 UserCommandApis = user.UserCommandApis
+TeamCommandApis = team.TeamCommandApis
