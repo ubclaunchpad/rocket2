5e7013d25f7e862ea910399badf160efe3bb9751
Robert Lin <robert@bobheadxi.dev>
2020-09-29 13:57:28 +0800

    permissions: make permissions comparable

--- app/model/permissions.py
+++ app/model/permissions.py
@@ -2,7 +2,34 @@
 from enum import Enum
 
 
-class Permissions(Enum):
+class OrderedEnum(Enum):
+    """
+    Comparable enum -
+    copied from https://docs.python.org/3/library/enum.html#orderedenum
+    """
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
+
+
+class Permissions(OrderedEnum):
     """Enum to represent possible permissions levels."""
 
     member = 1
