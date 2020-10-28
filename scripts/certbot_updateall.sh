3537b32e98f9afc40ff3fde7584039b198e873ec
Cheuk Yin Ng <cheukyin699@users.noreply.github.com>
2020-10-27 19:17:45 -0700

    Fix certbot auto-renew script (#581)
    Add `--webroot -w /usr/share/nginx/html` to let certbot add files there instead of doing something stupid.

--- scripts/certbot_updateall.sh
+++ scripts/certbot_updateall.sh
@@ -6,4 +6,4 @@ docker run --rm --name letsencrypt \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/usr/share/nginx/html:/usr/share/nginx/html" \
     certbot/certbot:latest \
-    renew --quiet
+    renew --quiet --webroot -w /usr/share/nginx/html
