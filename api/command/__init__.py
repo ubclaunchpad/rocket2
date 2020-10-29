a5c132364b83d19cef655f660490ff2cda4c3a16
Cheuk Yin Ng <cheukyin699@users.noreply.github.com>
2020-10-29 08:34:35 -0700

    tests: increase clarity by a marginal amount (#583)

--- api/command/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Encapsulate the common business logic of commands."""
-import api.command.core as core
-
-CommandApis = core.CommandApis
--- api/command/mixins/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Define the APIs of the various command types."""
-import api.command.mixins.user as user
-import api.command.mixins.team as team
-
-UserCommandApis = user.UserCommandApis
-TeamCommandApis = team.TeamCommandApis
