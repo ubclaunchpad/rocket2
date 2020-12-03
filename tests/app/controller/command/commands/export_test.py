f6346ebbf26a18238d6cd8762eee75f5c4106df2
Cheuk Yin Ng <chucksys88@gmail.com>
2020-12-02 17:07:11 -0800

    tests: add more export tests to satisfy cov

--- tests/app/controller/command/commands/export_test.py
+++ tests/app/controller/command/commands/export_test.py
@@ -30,10 +30,24 @@ class TestExportCommand(TestCase):
         self.t1 = Team('320484', 'aqua-scepter', 'Aqua Scepter')
         self.t1.add_member(self.u1.github_id)
 
+        self.t2 = Team('22234', 'tiger-dear', 'Shakespearean')
+
         self.db = MemoryDB(users=[self.u0, self.u1, self.admin, self.lead],
-                           teams=[self.t0, self.t1])
+                           teams=[self.t0, self.t1, self.t2])
 
         self.cmd = ExportCommand(self.db)
+
+    def test_get_help_from_bad_syntax(self):
+        resp, _ = self.cmd.handle('export emails hanrse', self.admin.slack_id)
+        self.assertEqual(resp, self.cmd.get_help('emails'))
+
+    def test_get_help_bad_syntax_all(self):
+        resp, _ = self.cmd.handle('export blah', self.admin.slack_id)
+        self.assertEqual(resp, self.cmd.get_help())
+
+    def test_lookup_error_user_calling(self):
+        resp, _ = self.cmd.handle('export emails', 'blah blah')
+        self.assertEqual(resp, ExportCommand.lookup_error)
 
     def test_get_all_emails(self):
         resp, _ = self.cmd.handle('export emails', self.admin.slack_id)
@@ -87,11 +101,31 @@ class TestExportCommand(TestCase):
         self.assertIn('Members who don\'t have an email:', resp)
         self.assertNotIn(ExportCommand.no_emails_missing_msg, resp)
 
+    def test_get_emails_no_users_in_team(self):
+        resp, _ = self.cmd.handle(
+            f'export emails --team {self.t2.github_team_name}',
+            self.admin.slack_id)
+        self.assertEqual(resp, ExportCommand.no_user_msg)
+
     def test_get_all_emails_char_limit_reached(self):
         old_lim = ExportCommand.MAX_CHAR_LIMIT
         ExportCommand.MAX_CHAR_LIMIT = 30
         resp, _ = self.cmd.handle('export emails', self.admin.slack_id)
         self.assertIn(ExportCommand.char_limit_exceed_msg, resp)
+
+        # reset things because python doesn't do that
+        ExportCommand.MAX_CHAR_LIMIT = old_lim
+
+    def test_get_all_emails_one_missing_char_limit_reached(self):
+        old_lim = ExportCommand.MAX_CHAR_LIMIT
+        ExportCommand.MAX_CHAR_LIMIT = 30
+        self.u0.email = ''
+        resp, _ = self.cmd.handle(
+            f'export emails',
+            self.lead.slack_id)
+        self.assertIn(self.u0.slack_id, resp)
+        self.assertIn('Members who don\'t have an email:', resp)
+        self.assertNotIn(ExportCommand.no_emails_missing_msg, resp)
 
         # reset things because python doesn't do that
         ExportCommand.MAX_CHAR_LIMIT = old_lim
